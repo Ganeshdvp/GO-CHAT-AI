9ad89427a8bef0a66a0acbe7863149157d282d2f
Ganeshdvp <ganeshcherupalli6565@gmail.com>
2025-12-15 17:43:30 +0530

    mobile menubar

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Main.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCode,
   faLightbulb,
   faPeopleGroup,
+  faBarsStaggered,
 } from "@fortawesome/free-solid-svg-icons";
 import { faCompass } from "@fortawesome/free-regular-svg-icons";
 import SignUp from "../SignUp/SignUp";
@@ -12,11 +13,59 @@ import SignIn from "../SignIn/SignIn";
 import { InputBottom } from "../inputBottom/InputBottom";
 import appStore from "../../constants/appStore";
 import { Chat } from "../chat/Chat";
+import {
+  faCircleInfo,
+  faClockRotateLeft,
+  faCommentDots,
+  faGear,
+  faPlus,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Main = () => {
   const [showForm, setShowForm] = useState("signup");
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const { showChatComponent } = appStore();
+  const {
+    setNewChat,
+    recentChats,
+    deleteRecentChats,
+    setActiveChat,
+    setShowChatComponent,
+    showChatComponent,
+  } = appStore();
+
+  const handleNewChat = () => {
+    setNewChat();
+    setIsMenuOpen(false);
+  };
+
+  const handleClick = (id) => {
+    setActiveChat(id);
+    setShowChatComponent(false);
+    setIsMenuOpen(false);
+  };
+
+  useEffect(() => {
+    if (recentChats.length === 0) {
+      setShowChatComponent(true);
+    }
+  }, [recentChats]);
+
+  useEffect(()=>{
+    const handleResize = ()=>{
+      if(window.innerWidth > 600){
+        setIsMenuOpen(false);
+      }
+    }
+    window.addEventListener("resize", handleResize);
+
+    
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  },[])
+
 
   if (showForm === "signup") {
     return (
@@ -39,13 +88,73 @@ const Main = () => {
   return (
     <div className="main">
       <div className="nav">
-        <p>GO-CHAT-AI</p>
+        <div className="nav-one">
+          <FontAwesomeIcon
+            icon={faBarsStaggered}
+            className="menu-logo"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          />
+          <p>GO-CHAT-AI</p>
+        </div>
         <img
           src="https://cdn-icons-png.flaticon.com/512/4113/4113045.png"
           alt="logo"
           onClick={() => setShowForm("signup")}
         />
       </div>
+
+      {isMenuOpen && (
+        <div className={`sidebar-mobile ${isMenuOpen ? "open" : ""}`}>
+          <div className="sidebar-mobile-top">
+            <div className="sidebar-mobile-new-chat" onClick={handleNewChat}>
+              <FontAwesomeIcon icon={faPlus} className="sidebar-mobile-plus-icon" />
+              <p>New Chat</p>
+            </div>
+            <div className="sidebar-mobile-recent">
+              <p className="sidebar-mobile-recent-tittle">Recent</p>
+              {recentChats.length > 0 ? (
+                recentChats.map((ele) => {
+                  return (
+                    <div className="sidebar-mobile-recent-entry" key={ele.id}>
+                      <div
+                        className="sidebar-mobile-recent-entry2"
+                        onClick={() => handleClick(ele.id)}
+                      >
+                        <FontAwesomeIcon icon={faCommentDots} />
+                        <p>{ele.recentChatText?.slice(0, 15)}...</p>
+                      </div>
+                      <FontAwesomeIcon
+                        icon={faTrash}
+                        className="sidebar-mobile-trash-icon"
+                        onClick={() => deleteRecentChats(ele.id)}
+                      />
+                    </div>
+                  );
+                })
+              ) : (
+                <p className="sidebar-mobile-no-recent-chats">No recent chats</p>
+              )}
+            </div>
+          </div>
+          <div className="sidebar-mobile-bottom">
+            <div className="sidebar-mobile-bottom-item sidebar-mobile-recent-entry-bottom">
+              <FontAwesomeIcon icon={faCircleInfo} style={{ color: "white" }} />
+              <p>Help</p>
+            </div>
+            <div className="sidebar-mobile-bottom-item sidebar-mobile-recent-entry-bottom">
+              <FontAwesomeIcon
+                icon={faClockRotateLeft}
+                style={{ color: "white" }}
+              />
+              <p>Activity</p>
+            </div>
+            <div className="sidebar-mobile-bottom-item sidebar-mobile-recent-entry-bottom">
+              <FontAwesomeIcon icon={faGear} style={{ color: "white" }} />
+              <p>Settings</p>
+            </div>
+          </div>
+        </div>
+      )}
 
       <div className="main-container">
         {showChatComponent ? (
